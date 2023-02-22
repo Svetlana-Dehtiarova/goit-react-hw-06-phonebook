08255a876b3ac3f234361471f62fa63de790b1e1
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2023-02-22 20:17:55 +0200

    Initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+import ContactList from './ContactList/ContactList';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+
+export function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
+    const isAdded = contacts.some(
+      ({ name }) => name.toLowerCase() === normalizedName
+    );
+
+    if (isAdded) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    setContacts(prevContacts => [...prevContacts, contact]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value.trim());
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
+
+  const visibleContacts = getVisibleContacts();
+
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        flexDirection: 'column',
+        fontSize: 18,
+        color: '#037B7B',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2 className={css.titleContacts}>Contacts</h2>
+      <div className={css.allContacts}>All contacts: {contacts.length}</div>
+      {contacts.length > 0 ? (
+        <>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            onDeleteContact={deleteContact}
+          />
+        </>
+      ) : (
+        <p>Phonebook is empty. Please add your first contact:)</p>
+      )}
     </div>
   );
-};
+}
